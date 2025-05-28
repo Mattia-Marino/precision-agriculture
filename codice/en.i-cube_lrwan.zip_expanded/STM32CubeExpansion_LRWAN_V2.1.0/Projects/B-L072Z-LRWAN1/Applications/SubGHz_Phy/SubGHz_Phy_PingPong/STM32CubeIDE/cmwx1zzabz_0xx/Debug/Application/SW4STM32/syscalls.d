9d081d95e49c3c7c165b88fde2f44dad865ed2b7
Mattia Marino <mattia.marino01@gmail.com>
2025-05-28 12:26:33 +0200

    Ping-Pong LoRA

---
+++ codice/en.i-cube_lrwan.zip_expanded/STM32CubeExpansion_LRWAN_V2.1.0/Projects/B-L072Z-LRWAN1/Applications/SubGHz_Phy/SubGHz_Phy_PingPong/STM32CubeIDE/cmwx1zzabz_0xx/Debug/Application/SW4STM32/syscalls.d
@@ -0,0 +1,2 @@
+Application/SW4STM32/syscalls.o: \
+ /home/mattia/University/Network\ Measurements/Project/en.i-cube_lrwan.zip_expanded/STM32CubeExpansion_LRWAN_V2.1.0/Projects/B-L072Z-LRWAN1/Applications/SubGHz_Phy/SubGHz_Phy_PingPong/STM32CubeIDE/syscalls.c
---
+++ codice/en.i-cube_lrwan.zip_expanded/STM32CubeExpansion_LRWAN_V2.1.0/Projects/B-L072Z-LRWAN1/Applications/SubGHz_Phy/SubGHz_Phy_PingPong/STM32CubeIDE/cmwx1zzabz_0xx/Debug/Utilities/stm32_mem.d
@@ -0,0 +1,14 @@
+Utilities/stm32_mem.o: \
+ /home/mattia/University/Network\ Measurements/Project/en.i-cube_lrwan.zip_expanded/STM32CubeExpansion_LRWAN_V2.1.0/Utilities/misc/stm32_mem.c \
+ /home/mattia/University/Network\ Measurements/Project/en.i-cube_lrwan.zip_expanded/STM32CubeExpansion_LRWAN_V2.1.0/Utilities/misc/stm32_mem.h \
+ ../../../Core/Inc/utilities_conf.h \
+ ../../../../../../../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../../../../../../../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../../../../../../../../Utilities/misc/stm32_mem.h \
+ ../../../../../../../../Utilities/misc/stm32_tiny_vsnprintf.h
+/home/mattia/University/Network\ Measurements/Project/en.i-cube_lrwan.zip_expanded/STM32CubeExpansion_LRWAN_V2.1.0/Utilities/misc/stm32_mem.h:
+../../../Core/Inc/utilities_conf.h:
+../../../../../../../../Drivers/CMSIS/Include/cmsis_compiler.h:
+../../../../../../../../Drivers/CMSIS/Include/cmsis_gcc.h:
+../../../../../../../../Utilities/misc/stm32_mem.h:
+../../../../../../../../Utilities/misc/stm32_tiny_vsnprintf.h:
